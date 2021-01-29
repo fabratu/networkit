409972e70bac0d3ff866098724b7a3da60b29654
Fabian Brandt-Tumescheit <fabian.brandt-tumescheit@informatik.hu-berlin.de>
2021-01-29 13:41:17 +0100

    Adds external test

--- .github/workflows/scripts/clang_tidy.sh
+++ .github/workflows/scripts/clang_tidy.sh
@@ -6,6 +6,7 @@ sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-11 9999
 sudo update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-11 9999
 $CXX --version
 clang-tidy --version
+
 mkdir debug_test && cd "$_"
 cmake -GNinja -DNETWORKIT_BUILD_TESTS=ON -DNETWORKIT_CLANG_TIDY=ON -DCMAKE_BUILD_TYPE=Debug ..
 ninja
